4f0eee9e62e436b12fd12020670e26b83dc59372
Ashley Pereira <ashleypereira70@gmail.com>
2018-10-05 15:14:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var greeting = "Hello, everybody!";
+//A string is a collection of characters (letters, numbers, and symbols) wrapped in single or double quotes 
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+//When we wrap strings in single or double quotes, we can join them together using the + operator
 
 var topic = "space";
 
 var conversation =`Let's talk about ${topic}.`;
+//When we wrap strings in back ticks, we can use placeholders (${}) and insert variables or evaluated JavaScript directly
